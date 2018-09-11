5f0499929d1e7660953326ca8564384d540074c8
amenpunk <ondasycircuitos@gmail.com>
2018-09-10 19:47:48 -0600

    funcionalidad jQuery

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-   //to do
+$(document).ready(function() {
+    alert("jQuery esta funcionando !!");
+    $('button').on('click',function(){
+        var selected = $('#place option:selected');
+        var value = selected.val();
+        var price = selected.data('price');
+        //console.log(value);
+        //console.log(value);
+        var color = selected.attr('data-price');
+    $('#resultado').css('background-color',color);
+
+    })
 });
